7ea54bf6c9a8424c4e470ca760c0a754f7a9ae2f
sukdong-kim <sdkim@hoseo.edu>
2020-11-06 16:46:49 +0900

    Example for javafx application

--- mffx4/src/application/Main.java
+++ mffx4/src/application/Main.java
@@ -12,7 +12,8 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,600,400);
+			Scene scene = new Scene(root,600,600);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
--- mffx4/src/application/SampleController.java
+++ mffx4/src/application/SampleController.java
@@ -40,6 +40,11 @@ public class SampleController implements Initializable {
     @FXML
     private TextArea ta;
 
+    String sel1 = "";
+    String sel2 = "";
+    int num1=0;
+    int num2=0;
+    
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		// TODO Auto-generated method stub
@@ -49,36 +54,72 @@ public class SampleController implements Initializable {
 	
 	}
     
- 
+    @FXML
+    void onClickbombo(ActionEvent event) {
+    	String sel = comboBox.getValue() ;
+    	sel1 += sel ;
+    	if(sel.equals("페페로니 피자, 15,000원"))
+    		num1 = 15000;
+    	else if(sel.equals("포테이토 피자, 15,000원"))
+    		num1 = 15000;
+    	else if(sel.equals("불고기 피자, 18,000원"))
+    		num1 = 18000;
+    	else if(sel.equals("콤보 피자, 20,000원"))
+    		num1 = 20000;
+    	
+    }
     
     @FXML
     void onClickb1(ActionEvent event) {
-
+    	ta.appendText(sel1+"\n");
+    	ta.appendText(sel2);
+    	ta.appendText("\ntotal = "+(num1+num2) +"원 입니다.");
     }
 
     @FXML
     void onClickb2(ActionEvent event) {
-
+    	sel1="";
+    	sel2="";
+    	ta.setText("");
+    	if(ch1.isSelected())
+    		ch1.fire();
+    	if(ch2.isSelected())
+    		ch2.fire();
+    	if(ch3.isSelected())
+    		ch3.fire();
+    	num1=0;
+    	num2=0;
+   
+ 
     }
-
     @FXML
+    
     void onClickb3(ActionEvent event) {
-
+    	System.exit(0);
     }
 
     @FXML
     void onClickch1(ActionEvent event) {
-
+    	if(ch1.isSelected()) {
+    		sel2 += ch1.getText() + "\n";
+    		num2+=2000;
+    	}
     }
 
     @FXML
     void onClickch2(ActionEvent event) {
-
+    	if(ch2.isSelected()) {
+    		sel2 += ch2.getText() + "\n";
+    		num2+=1000;
+    	}
     }
 
     @FXML
     void onClickch3(ActionEvent event) {
-
+    	if(ch3.isSelected()) {
+    		sel2 += ch3.getText() + "\n";
+    		num2+=500;
+    	}
     }
 	
 }
